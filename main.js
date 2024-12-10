734a61206d80e113b15ee649514d32778dd261d2
Ali3990 <alan.li.3990@gmail.com>
2024-12-10 15:36:32 -0800

    added eventListeners for both numbers and operators.

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 
-const numbers = document.querySelectorAll(".number") // store all the button number elements in Array
+const output = document.querySelector("#display");
+const numbers = document.querySelectorAll(".number"); // store all the button number elements in Array
+const operators = document.querySelectorAll(".op"); // store all the operators
+const clear = document.querySelector(".clear");
 
 // operators
 const add = (x, y) => x + y;
@@ -9,10 +12,10 @@ const divide = (x,y) => x / y;
 
 let displayValue = "0";
 
-// work on this first, how do I store the number? and after an operator is hit, how do I 
-let firstNum;    // first number
-let operator;      // operator
-let secondNum;    // second number
+let firstNum = "";    
+let operator = "";      
+let secondNum = "";
+let currResult = ""; // ongoing tab of current calculated value
 
 
 //start with storing numbers up to multiple digits into one variable
@@ -20,11 +23,27 @@ numbers.forEach((number) => {
     number.addEventListener("click", e => {
         if (operator === "") { // Read first number if no operator has been set
             firstNum += e.target.innerText;
-            console.log(firstNum)
-        } else { //Read second number
+            output.value = firstNum;
+        } 
+        else { // If operator is set, read second number
             secondNum += e.target.innerText;
-            console.log(secondNum)
+            output.value = secondNum;
         }
     })
 });
 
+operators.forEach((op) => {
+    op.addEventListener("click", e =>{
+        operator = e.target.innerText;
+    })
+})
+
+
+// Erases all the stored values and text value within the display window.
+const clearAll = function(){
+    firstNum = ""
+    secondNum = ""
+    operator = ""
+    output.value = ""
+    displayValue = "0"
+}
