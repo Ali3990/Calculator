47041428c02b1adfeb839b799dcb7c359fa1d991
Alan Li <61648323+Ali3990@users.noreply.github.com>
2024-12-10 22:33:53 -0800

    Fixed calc function with breaks and allClear function

--- main.js
+++ main.js
@@ -2,7 +2,8 @@
 const output = document.querySelector("#display");
 const numbers = document.querySelectorAll(".number"); // store all the button number elements in Array
 const operators = document.querySelectorAll(".op"); // store all the operators
-const clear = document.querySelector(".clear");
+const clear = document.querySelector(".clear")
+
 
 // operators
 const add = (x, y) => x + y;
@@ -11,23 +12,23 @@ const multiply = (x,y) => x * y;
 const divide = (x,y) => x / y;
 
 let displayValue = "0";
-
 let firstNum = "";    
 let operator = "";      
 let secondNum = "";
 let currResult = ""; // ongoing tab of current calculated value
 
 
-//start with storing numbers up to multiple digits into one variable
 numbers.forEach((number) => {
     number.addEventListener("click", event => {
         if (operator === "") { // Read first number if no operator has been set
             firstNum += event.target.innerText;
             output.value = firstNum;
+            console.log(firstNum)
         } 
         else { // If operator is set, read second number
             secondNum += event.target.innerText;
             output.value = secondNum;
+            console.log(secondNum)
         }
     })
 });
@@ -35,6 +36,7 @@ numbers.forEach((number) => {
 operators.forEach((op) => {
     op.addEventListener("click", event =>{
         operator = event.target.innerText;
+        console.log(operator)
     })
 })
 
@@ -43,21 +45,30 @@ const calc = function(){
     b = Number(secondNum)
     switch (operator){
         case '+':
-            output.value = add(a, b)
+            output.value = a + b;
+            firstNum = a + b;
+            break;
         case '-':
-            output.value = subtract(a, b)
+            output.value = subtract(a, b);
+            firstNum = subtract(a, b);
+            break;
         case '*':
-            output.value = multiply(a, b)
+            output.value = multiply(a, b);
+            firstNum = multiply(a, b);
+            break;
         case '/':
-            output.value = divide(a, b)
+            output.value = divide(a, b);
+            firstNum = divide(a, b);
+            break;
     }
 }
 
 // Erases all the stored values and text value within the display window.
-const clearAll = function(){
+const allClear = function(){
     firstNum = ""
     secondNum = ""
     operator = ""
     output.value = ""
     displayValue = "0"
 }
+
